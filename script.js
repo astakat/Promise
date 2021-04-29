cd3eea931550f3d92450e64fff4341ae20029b8f
Asta Kat <astakat@gmail.com>
2021-04-29 10:48:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+/*
+Exercise 1:
+Write a function testNum that takes a number as an argument and returns a Promise that tests if the value is less than or greater than the value 10. Log the result to the console.
+*/
+
+const testNum = (num) => {
+  return new Promise((resolve, reject) => {
+    if (num > 10) {
+      resolve(num + " is greater then 10");
+    } else {
+      reject(num + " is less then 10");
+    }
+  });
+};
+
+testNum()
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+// It's not actaully catching an error...
+
+/*
+Exercise 2:
+Write two functions that use Promises that you can chain! The first function, makeAllCaps(), will take in an array of words and capitalize them, and then the second function, sortWords(), will sort the words in alphabetical order. If the array contains anything but strings, it should throw an error.
+Then call these functions by *chaining* the promises
+*/
+
+const makeAllCaps = (words) => {
+  return new Promise((resolve, reject) => {
+    if (
+      words.every((word) => {
+        return typeof word === "string";
+      })
+    ) {
+      resolve(
+        sortWords(
+          words.map((word) => {
+            return word.toUpperCase();
+          })
+        )
+      );
+    } else {
+      reject("Not a string!");
+    }
+  });
+};
+
+const sortWords = (words) => {
+  return new Promise((resolve, reject) => {
+    if (words) {
+      resolve(words.sort());
+    } else {
+      reject("Strings only!");
+    }
+  });
+};
+
+const arrayOfWords = ["cucumber", "tomatos", "avocado"];
+const complicatedArray = ["cucumber", 44, true];
+
+makeAllCaps(arrayOfWords)
+  .then(sortWords(arrayOfWords))
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+
+makeAllCaps(complicatedArray)
+  .then(sortWords(complicatedArray))
+  .then((result) => console.log(result))
+  .catch((error = console.log(error)));
+
+// call both functions, chain them together and log the result to the console
